1461c0d2cba99242b2224c66bd49980b83040211
Belvin Shandy Aurora <belshandy1209@gmail.com>
2022-08-10 14:38:54 +0700

    DELETE done ✅

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,22 @@ $("#update_user").submit(function (event) {
     alert("Data Berhasil Diupdate!");
   });
 });
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function (event) {
+    var id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Apakah Anda Yakin?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Berhasil Dihapus!");
+        location.reload();
+      });
+    }
+  });
+}
